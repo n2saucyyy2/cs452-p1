3383b9f8cae6698e8a0e8a9b1fa44306f2f7a054
Nolan Stetz <122702059+n2saucyyy2@users.noreply.github.com>
2024-09-15 18:48:11 +0000

    Added Lab.c and Lab.h starter code along with some minor changes from main.c code in class

--- app/main.c
+++ app/main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <bits/getopt_core.h>
+#include <lab.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
   printf("hello world\n");
-  return 0;
+
+  int c;
+
+  while((c = getopt(argc, argv, "abc:")) != -1)
+    switch(c)
+  {
+    case 'a':
+     printf("get a here");
+      break;
+
+    case 'b':
+      printf("get b here");
+      break;
+
+    case 'c':
+      printf("get c here");
+      break;
+
+    case '?':
+      if(isprint(getopt))
+        fprintf(stderr, "Unknown Option", optopt);
+      else
+        fprintf(stderr, "Unkown Option", optopt);
+      return 1;
+
+    default:
+      abort();
+  }
 }
